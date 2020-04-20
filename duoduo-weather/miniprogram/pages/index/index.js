38f463c6f638e881fe9fe57692c0d98c4d3fdcfa
zhangzhenlin <chenlim233@163.com>
2020-04-20 18:01:01 +0800

    用户授权获取位置信息

--- duoduo-weather/miniprogram/pages/index/index.js
+++ duoduo-weather/miniprogram/pages/index/index.js
@@ -7,7 +7,15 @@ Page({
   },
 
   onLoad: function() {
-    
+    // 获取用户的位置信息
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        console.log(latitude,longitude)
+      }
+    })
   },
 
 })
